c03865161adc51916d52b628bcaf4ba1134fdf0c
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-04-17 18:48:23 -0300

    feat: (shared) - create enums

---
+++ src/app/shared/enums/index.ts
@@ -0,0 +1,3 @@
+export * from './user-role.enum';
+export * from './token-type.enum';
+export * from './api-key-name.enum';
---
+++ src/app/shared/enums/token-type.enum.ts
@@ -0,0 +1,4 @@
+export enum TokenType {
+    refresh = 'refresh',
+    recovery = 'recovery',
+}
