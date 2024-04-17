88112e084efb1f7087bc2d7d0540a7d1a7fb64e3
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-04-17 18:30:47 -0300

    feat: (setup) - first commit

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableShutdownHooks();
+  app.enableCors({
+    origin: '*',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(3000);
+}
+bootstrap();
