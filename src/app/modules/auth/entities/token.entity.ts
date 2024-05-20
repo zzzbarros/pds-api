bbfba32c81ecab4fb77de9e904edfc072e549b10
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-05-20 00:12:36 -0300

    feat: (auth) -  update token entity

--- src/app/modules/auth/entities/token.entity.ts
+++ src/app/modules/auth/entities/token.entity.ts
@@ -1,66 +1,77 @@
-import { BaseEntity, TokenType } from 'src/app/shared';
+import { randomUUID } from 'crypto';
+import { BaseEntity, IBaseConstructor, TokenTypeEnum } from 'src/app/shared';
+
+interface ITokenEntityConstructor extends IBaseConstructor {
+  type: TokenTypeEnum;
+  userId: number;
+  isValid?: boolean;
+  expiresIn?: Date;
+  token?: string;
+  id?: number;
+  uuid?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
 
 export class TokenEntity extends BaseEntity {
   private token: string;
-  private type: TokenType;
-  private userId: string;
+  private type: TokenTypeEnum;
+  private userId: number;
   private expiresIn: Date;
   private isValid: boolean;
 
-  getExpiresIn(): Date {
-    return this.expiresIn;
-  }
-
-  getToken(): string {
-    return this.token;
-  }
-
-  getUserId(): string {
-    return this.userId;
-  }
-
-  getType(): TokenType {
-    return this.type;
-  }
-
-  getIsValid(): boolean {
-    return this.isValid;
-  }
-
-  setToken(token?: string): void {
-    const newToken = this.generateRandomToken();
-    this.token = token ?? newToken;
-  }
-
-  setType(type: TokenType): void {
+  constructor({
+    token,
+    userId,
+    expiresIn,
+    type,
+    isValid = true,
+    id,
+    uuid,
+    createdAt,
+    updatedAt,
+  }: ITokenEntityConstructor) {
+    super(id, uuid, createdAt, updatedAt);
+    this.expiresIn = this.generateExpiresDate(expiresIn);
+    this.token = this.generateRandomToken(token);
+    this.userId = userId;
+    this.isValid = isValid;
     this.type = type;
   }
 
-  setUserId(userId: string): void {
-    this.userId = userId;
+  public invalid() {
+    this.isValid = false;
   }
 
-  setExpiresIn(expiresIn?: Date) {
+  public getExpiresIn(): Date {
+    return this.expiresIn;
+  }
+
+  public getToken(): string {
+    return this.token;
+  }
+
+  public getUserId(): number {
+    return this.userId;
+  }
+
+  public getType(): TokenTypeEnum {
+    return this.type;
+  }
+
+  public getIsValid(): boolean {
+    return this.isValid;
+  }
+
+  private generateRandomToken(token?: string): string {
+    if (token) return token;
+    return randomUUID();
+  }
+
+  private generateExpiresDate(expiresIn?: Date) {
+    if (expiresIn) return expiresIn;
     const dayInMilliseconds = 86400000;
-    const weekInMilliseconds = dayInMilliseconds * 7;
     const today = new Date();
-    const expiresInDate = new Date(today.getTime() + weekInMilliseconds);
-
-    this.expiresIn = expiresIn ?? expiresInDate;
-  }
-
-  setIsValid(isValid = true) {
-    this.isValid = isValid;
-  }
-
-  private generateRandomToken(): string {
-    const length = 32;
-    const characters =
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let token = '';
-    for (let i = 0; i < length; i++) {
-      token += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return token;
+    return new Date(today.getTime() + dayInMilliseconds);
   }
 }
