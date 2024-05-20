2c36c3f738d14f0633d0b8c5b6d79e36c3f742c3
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-05-20 00:09:42 -0300

    feat: (users) - update create user use case

--- src/app/modules/user/repositories/user.repository.ts
+++ src/app/modules/user/repositories/user.repository.ts
@@ -1,7 +1,9 @@
 import { UserEntity } from '../entities';
 
 export interface IUserRepository {
-  create(user: UserEntity): Promise<void>;
+  create(user: UserEntity): Promise<{ id: number }>;
+  update(user: UserEntity): Promise<void>;
   findByEmail(email: string): Promise<UserEntity | null>;
   findByUuid(uuid: string): Promise<UserEntity | null>;
+  findById(id: number): Promise<UserEntity | null>;
 }
--- src/app/modules/user/usecases/create-user.usecase.ts
+++ src/app/modules/user/usecases/create-user.usecase.ts
@@ -1,35 +1,42 @@
 import { ConflictException, Inject, Injectable } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 import { randomUUID } from 'crypto';
 import { CreateUserDto } from '../dtos';
 import { UserEntity } from '../entities';
-import { IUserRepository } from '../repositories';
-import { ISecurityRepository } from '../../auth';
-import { IQueueRepository } from '../../queues';
-import { USER_ROLE_ENUM, type BaseUseCase } from 'src/app/shared';
+import type { IUserRepository } from '../repositories';
+import type { ISecurityRepository } from '../../auth';
+import type { IQueueRepository } from '../../queues';
+import { EventsEnum, UserRoleEnum, type IBaseUseCase } from 'src/app/shared';
 
 @Injectable()
-export class CreateUserUseCase implements BaseUseCase {
+export class CreateUserUseCase implements IBaseUseCase {
   constructor(
     @Inject('IUserRepository')
-    private readonly IUserRepository: IUserRepository,
+    private readonly userRepository: IUserRepository,
     @Inject('IQueueRepository')
-    private readonly IQueueRepository: IQueueRepository,
+    private readonly queueRepository: IQueueRepository,
     @Inject('ISecurityRepository')
     private readonly securityRepository: ISecurityRepository,
+    private readonly eventEmitter: EventEmitter2,
   ) {}
 
   async execute(input: CreateUserDto): Promise<void> {
     const { name, email } = input;
     await this.validateEmailUniqueness(email);
-    const role = USER_ROLE_ENUM.COACH;
+    const role = UserRoleEnum.COACH;
     const password = await this.generatePassword();
     const user = new UserEntity({ name, email, password, role });
-    await this.IUserRepository.create(user);
-    await this.IQueueRepository.sendMailToQueue({ email, name });
+    const { id: userId } = await this.userRepository.create(user);
+    await this.queueRepository.sendMailToQueue({
+      email,
+      name,
+      userId,
+    });
+    this.eventEmitter.emit(EventsEnum.CREATE_USER, { userId, email, name });
   }
 
   private async validateEmailUniqueness(email: string): Promise<void> {
-    const existingUser = await this.IUserRepository.findByEmail(email);
+    const existingUser = await this.userRepository.findByEmail(email);
     if (existingUser) {
       throw new ConflictException({
         title: 'Não foi possível criar a conta!',
