d4f7efbae59ad7574ed315c1574cf69500ac854a
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-05-26 20:20:50 -0300

    feat: (athlete) -  create athlete dto

---
+++ src/app/modules/athlete/dtos/create-athlete.dto.ts
@@ -0,0 +1,26 @@
+import {
+  IsDateString,
+  IsEmail,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateAthleteDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsDateString()
+  birthday: Date;
+
+  @IsNumber()
+  @IsOptional()
+  weight?: number;
+
+  @IsNumber()
+  @IsOptional()
+  height?: number;
+}
---
+++ src/app/modules/athlete/dtos/index.ts
@@ -0,0 +1,1 @@
+export * from './create-athlete.dto'
