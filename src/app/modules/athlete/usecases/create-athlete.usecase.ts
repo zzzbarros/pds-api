2e3f48c0d0aea4d52454f93d7a880eb72f4e5449
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-05-26 20:21:33 -0300

    feat: (athlete) -  create athlete use case

---
+++ src/app/modules/athlete/usecases/create-athlete.usecase.ts
@@ -0,0 +1,30 @@
+import { ConflictException, Inject, Injectable } from '@nestjs/common';
+import { CreateAthleteDto } from '../dtos';
+import { AthleteEntity } from '../entities';
+import type { IAthleteRepository } from '../repositories';
+import type { IBaseUseCase } from 'src/app/shared';
+
+@Injectable()
+export class CreateAthleteUseCase implements IBaseUseCase {
+  constructor(
+    @Inject('IAthleteRepository')
+    private readonly athleteRepository: IAthleteRepository,
+  ) {}
+
+  async execute(input: CreateAthleteDto): Promise<void> {
+    await this.valideEmail(input.email);
+    const athlete = new AthleteEntity(input);
+    await this.athleteRepository.create(athlete);
+  }
+
+  private async valideEmail(email: string) {
+    const existentEmail = await this.athleteRepository.findByEmail(email);
+    if (existentEmail) {
+      throw new ConflictException({
+        title: 'E-mail já cadastrado. ',
+        message:
+          'Não foi possível cadastrar o atleta. Verifique e tente novamente...',
+      });
+    }
+  }
+}
