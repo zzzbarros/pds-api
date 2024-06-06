2bd6cb26a96274ecedb6583975984da34238fc61
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-06-06 14:33:08 -0300

    feat: (training) - create training entities

---
+++ src/app/modules/training/entities/training.entity.ts
@@ -0,0 +1,81 @@
+import { BaseEntity, type IBaseConstructor } from 'src/app/shared';
+import { TrainingTypeEntity } from '../../training-type';
+
+interface IConstructor extends IBaseConstructor {
+  athleteId: number;
+  date: Date;
+  trainingTypeId: number;
+  duration: number;
+  pse: number;
+  psr: number;
+  description?: string;
+  trainingType?: TrainingTypeEntity;
+}
+
+export class TrainingEntity extends BaseEntity {
+  private athleteId: number;
+  private date: Date;
+  private trainingTypeId: number;
+  private duration: number;
+  private pse: number;
+  private psr: number;
+  private description?: string;
+  private trainingType?: TrainingTypeEntity;
+
+  constructor({
+    athleteId,
+    date,
+    trainingTypeId,
+    duration,
+    pse,
+    psr,
+    description,
+    id,
+    uuid,
+    createdAt,
+    updateAt,
+    trainingType,
+  }: IConstructor) {
+    super(id, uuid, createdAt, updateAt);
+    this.athleteId = athleteId;
+    this.date = date;
+    this.trainingTypeId = trainingTypeId;
+    this.duration = duration;
+    this.description = description;
+    this.pse = pse;
+    this.psr = psr;
+    this.trainingType = trainingType;
+  }
+
+  public getAthleteId(): number {
+    return this.athleteId;
+  }
+
+  public getTrainingTypeId(): number {
+    return this.trainingTypeId;
+  }
+
+  public getDate(): Date {
+    return this.date;
+  }
+
+  public getDescription(): string {
+    return this.description;
+  }
+
+  public getDuration(): number {
+    return this.duration;
+  }
+
+  public getPSE(): number {
+    return this.pse;
+  }
+
+  public getPSR(): number {
+    return this.psr;
+  }
+
+  public getTrainingType(): TrainingTypeEntity {
+    return this.trainingType;
+  }
+}
