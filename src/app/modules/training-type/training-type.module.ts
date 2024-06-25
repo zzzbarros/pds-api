b431dfaa287ea858e21e3b409e69ed2c74651a3c
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-06-25 20:29:37 -0300

    feat: (training-type) - update training type module

--- src/app/modules/training-type/training-type.module.ts
+++ src/app/modules/training-type/training-type.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { TrainingTypeController } from './controllers';
 import {
   CreateTrainingTypeUseCase,
+  FindTrainingTypeUseCase,
   GetAllTrainingTypesUseCase,
   ListTrainingTypeUseCase,
+  UpdateTrainingTypeStatusUseCase,
+  UpdateTrainingTypeUseCase,
 } from './usecases';
 import { TrainingTypePostgresRepository } from 'src/infra/databases/orms/prisma/postgres';
 
@@ -13,6 +16,9 @@ import { TrainingTypePostgresRepository } from 'src/infra/databases/orms/prisma/
     CreateTrainingTypeUseCase,
     ListTrainingTypeUseCase,
     GetAllTrainingTypesUseCase,
+    UpdateTrainingTypeUseCase,
+    UpdateTrainingTypeStatusUseCase,
+    FindTrainingTypeUseCase,
     {
       provide: 'ITrainingTypeRepository',
       useClass: TrainingTypePostgresRepository,
--- src/app/modules/training-type/usecases/index.ts
+++ src/app/modules/training-type/usecases/index.ts
@@ -1,3 +1,6 @@
 export * from './create-training-type.usecase';
 export * from './list-training-type.usecase';
 export * from './get-all-training-types.usecase';
+export * from './find-training-type.usecase';
+export * from './update-training-type.usecase';
+export * from './update-training-type-status.usecase';
