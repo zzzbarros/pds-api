671d0a1f16cadd935fca5d8eac67eb21b968bb6d
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-06-29 22:52:39 -0300

    build: (app) - update pm2 instances limit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'training-track',
       script: './dist/main.js',  // Caminho para o arquivo principal da sua aplicação
-      instances: 2,  // Maximiza o uso de CPU
+      instances: 1,  // Maximiza o uso de CPU
       exec_mode: 'cluster',  // Modo cluster para melhor performance e disponibilidade
       env: {
         NODE_ENV: 'production',  // Ambiente de produção
