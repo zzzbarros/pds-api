05f86eb2664d4feab678ab269bd944b6e3b47166
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-07-06 12:58:23 -0300

    feat: (mail) - update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { PrismaModule } from 'src/infra/databases/orms/prisma/prisma.module';
 import { UserModule } from './modules/user/user.module';
 import { QueueModule } from './modules/queues/queue.module';
 import { SecurityModule } from 'src/infra/security/security.module';
-import { MailModule } from './modules/mail/mail.module';
 import { AthleteModule } from './modules/athlete/athlete.module';
 import { TrainingTypeModule } from './modules/training-type/training-type.module';
 import { TrainingPlanningModule } from './modules/training-planning/training-planning.module';
@@ -30,7 +29,6 @@ import { MonitoringModule } from './modules/monitory/monitoring.module';
     PrismaModule,
     SecurityModule,
     // ScheduleModule,
-    MailModule,
     AthleteModule,
     TrainingTypeModule,
     TrainingPlanningModule,
