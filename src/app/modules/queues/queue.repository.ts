a32924901f201dc9420393986acaea05afb27a3a
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-07-07 00:46:00 -0300

    feat: (well-being) - update queue repository

--- src/app/modules/queues/queue.repository.ts
+++ src/app/modules/queues/queue.repository.ts
@@ -1,6 +1,7 @@
 import { ReceiveMailFromQueueDto, SendMailToQueueDto } from './queue.dto';
 
 export interface IQueueRepository {
+  sendBatchMailToQueue(data: SendMailToQueueDto[]): Promise<void>;
   sendMailToQueue(data: SendMailToQueueDto): Promise<void>;
   receiveMailFromQueue(): Promise<ReceiveMailFromQueueDto>;
   removeMessage(receiptHandle: string): Promise<void>;
