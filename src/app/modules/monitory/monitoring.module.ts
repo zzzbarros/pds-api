091ac651ce7359174d8531cbaed83cc65854276c
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-07-07 00:45:50 -0300

    feat: (well-being) - update monitoring module

--- src/app/modules/monitory/monitoring.module.ts
+++ src/app/modules/monitory/monitoring.module.ts
@@ -1,15 +1,24 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { MonitoringController } from './controllers';
-import { MonotonyMonitoringUseCase, WeekMonitoringUseCase } from './usecases';
-import { AthleteModule } from '../athlete/athlete.module';
-import { TrainingPlanningModule } from '../training-planning/training-planning.module';
-import { TrainingModule } from '../training/training.module';
+import {
+  SendWellBeingToAthletesUseCase,
+  CaptureAthleteWellBeingUseCase,
+  MonotonyMonitoringUseCase,
+  WeekMonitoringUseCase,
+} from './usecases';
 import { UpdateWeekLoadListener } from './listeners';
+import { AthleteModule } from '../athlete/athlete.module';
+import { TrainingModule } from '../training/training.module';
+import { QueueModule } from '../queues/queue.module';
+import { TrainingPlanningModule } from '../training-planning/training-planning.module';
 import { MonitoryPostgresRepository } from 'src/infra/databases/orms/prisma/postgres/repositories';
+import { WellBeingPostgresRepository } from 'src/infra/databases/orms/prisma/postgres/repositories/well-being.repository';
+import { MonitoryTokenPostgresRepository } from 'src/infra/databases/orms/prisma/postgres/repositories/monitory-token.repository';
 
 @Module({
   controllers: [MonitoringController],
   imports: [
+    QueueModule,
     forwardRef(() => AthleteModule),
     forwardRef(() => TrainingModule),
     forwardRef(() => TrainingPlanningModule),
@@ -18,10 +27,21 @@ import { MonitoryPostgresRepository } from 'src/infra/databases/orms/prisma/post
     WeekMonitoringUseCase,
     MonotonyMonitoringUseCase,
     UpdateWeekLoadListener,
+    SendWellBeingToAthletesUseCase,
+    CaptureAthleteWellBeingUseCase,
     {
       provide: 'IMonitoryRepository',
       useClass: MonitoryPostgresRepository,
     },
+    {
+      provide: 'IWellBeingRepository',
+      useClass: WellBeingPostgresRepository,
+    },
+    {
+      provide: 'IMonitoryTokenRepository',
+      useClass: MonitoryTokenPostgresRepository,
+    },
   ],
+  exports: [SendWellBeingToAthletesUseCase],
 })
 export class MonitoringModule {}
