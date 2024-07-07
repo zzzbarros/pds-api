45123dae9088642eeeda7cee1425b7132ce8d92a
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-07-07 12:40:25 -0300

    feat: (well-being) - create get well being monitoring use case

--- src/app/modules/monitory/usecases/index.ts
+++ src/app/modules/monitory/usecases/index.ts
@@ -1,4 +1,5 @@
-export * from './week-monitoring.usecase';
+export * from './get-week-monitoring.usecase';
 export * from './monotony-monitoring.usecase';
 export * from './send-well-being-to-athletes.usecase';
 export * from './capture-athlete-well-being.usecase';
+export * from './get-well-being-monitoring.usecase';
